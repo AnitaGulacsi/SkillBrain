de28f0a1e84c714496c924356f308cc7f1624462
Anita Gulacsi <gulacsi_anita@yahoo.com>
2023-08-14 15:42:44 +0300

    Do exercises

--- Exercises/Array&Loops/increDecreForLoops.js
+++ Exercises/Array&Loops/increDecreForLoops.js
@@ -15,14 +15,59 @@
 // Implement a function totalBirdCount that accepts an array that contains the bird count per day.
 //     It should return the total number of birds that you counted.
 
+let totalBirdCount = (birdsPerDay) => {
+    let total = 0 
+    for (let i = 0; i < birdsPerDay.length; i++) {
+        total += birdsPerDay[i];
+    }
+    return total;
+};
 
+let birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+console.log("The total birds " + totalBirdCount(birdsPerDay));
 
 // 2. Calculate the number of visiting birds in a specific week
 // Now that you got a general feel for your bird count numbers, you want to make a more fine-grained analysis.
 // Implement a function birdsInWeek that accepts an array of bird counts per day and a week number.
 //     It returns the total number of birds that you counted in that specific week.You can assume weeks are always tracked completely.
 
+let birdsInWeek = (birdsPerDay , weekDetails) => {
+    let sumPerWeek = [];
+    let weekDays = 7 
+    let sum = 0;
+    let remainingDays = birdsPerDay.length % weekDays;  
+    let totalWeek = birdsPerDay.length / weekDays;
+    let isCompletedWeek = remainingDays === 0;
+    
+    for (let i = 0; i < birdsPerDay.length; i++) {  
+        sum += birdsPerDay[i];
+        if ((i + 1) % weekDays === 0) {
+            sumPerWeek.push(sum);
+            sum = 0;
+        } 
+    }
 
+    if (!isCompletedWeek) {
+        console.log("You have " + Math.floor(totalWeek) + " weeks and an uncompleted week witch have just " + remainingDays + " days");
+    } else {
+        console.log("You have " + totalWeek + " weeks");
+    }
+
+    let thisWeekDetails = sumPerWeek[weekDetails - 1];
+    
+    if (weekDetails == 1) {
+        console.log("You chose the 1st week");
+    } else if (weekDetails == 2) {
+        console.log("You chose the second week");
+    } else {
+        console.log("You chose the " + weekDetails + "th week");
+    }
+
+    return thisWeekDetails;
+};
+
+let weekDetails = 2;
+console.log(birdsInWeek(birdsPerDay , weekDetails));
 
 // 3. Fix a counting mistake
 // You realized that all the time you were trying to keep track of the birds, there was one hiding in a far corner of the garden. 
@@ -31,3 +76,17 @@
 //     in your garden on the first day that you tracked in your list.
 // Given this new information, write a function fixBirdCountLog that takes an array of birds counted per day as an argument. 
 //     It should correct the counting mistake and return the modified array.
+
+let fixBirdCountLog = (birdsPerDay) => {
+    let fixCountLog = [];
+
+    for (let i = 0; i < birdsPerDay.length; i++) {
+        let fixCount = birdsPerDay[i] + 1;
+        fixCountLog.push(fixCount);
+    }
+
+    return fixCountLog;
+
+};
+
+console.log(fixBirdCountLog(birdsPerDay));
