a14d5dde70a21d8a47a1731ebc3d55208380f312
Anita Gulacsi <gulacsi_anita@yahoo.com>
2023-09-06 09:05:15 +0300

    in buildSign  function, I changed the return data syntax.

--- Exercises/Functions/templateStrTernaryOpr.js
+++ Exercises/Functions/templateStrTernaryOpr.js
@@ -6,7 +6,7 @@
 // Implement the function buildSign(occasion, name) that accepts a string as the occasion parameter and a string holding someone's name as the name parameter. 
 // The two parameters will be embedded into a template string to output the message on the sign.
 
-let buildSign = (occasion , name) => ("Happy " + occasion + " " + name) ;
+let buildSign = (occasion , name) => (`Happy ${occasion} ${name}`) ;
 
 console.log(buildSign("Birthday" , "Rob"));
 
