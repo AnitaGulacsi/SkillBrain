b617d55298bf08d9bb047cda3be2d7ce5463476c
Anita Gulacsi <gulacsi_anita@yahoo.com>
2023-09-06 12:39:20 +0300

    first steps/make the task-card

--- Homeworks/React/Task-Manager/src/App.js
+++ Homeworks/React/Task-Manager/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
+import TaskCard from "./components/task-card/TaskCrad";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world!</h1>
-        <p>Start learning React!</p>
-      </header>
+    <div className="app-container">
+        <TaskCard/>
     </div>
   );
 }
