e9a9f9bcfc0f12f4afdfaa34e3966e4f97e59831
Anita Gulacsi <gulacsi_anita@yahoo.com>
2023-09-07 11:23:59 +0300

    fixed the code

--- Homeworks/React/Task-Manager/src/components/task-card/TaskCard.jsx
+++ Homeworks/React/Task-Manager/src/components/task-card/TaskCard.jsx
@@ -1,21 +1,25 @@
 import "./TaskCard.css";
 
 function TaskCard() {
-    return (
-    <div className="task-card-container">
-        <div className="header">
-            <p>T-1</p>
-            <button className="status-btn">Todo</button>
+  return (
+    <div className="card-wrapper">
+      <div className="card-header">
+        <p className="task-id">T-1</p>
+        <div className="badge">
+          <p>Todo</p>
         </div>
-        <div className="content">
-            <p>Create a Design System for Enum Workspace.</p>
+      </div>
+      <div className="card-content">
+        <p>Create a Design System for Enum Workspace.</p>
+      </div>
+      <div className="card-footer">
+        <div className="due-date">
+          <p>Due Date</p>
+          <p>24/ 01 /2021</p>
         </div>
-        <div className="footer">
-            <p>Due Date</p>
-            <p>24/ 01 /2021</p>
-        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default TaskCard;
