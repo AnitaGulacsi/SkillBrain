53d3cbd3d1d3294b633c2072cd9fa16cffd31bbd
Anita Gulacsi <gulacsi_anita@yahoo.com>
2023-09-08 12:10:49 +0300

    add components

--- Homeworks/React/Task-Manager/src/App.js
+++ Homeworks/React/Task-Manager/src/App.js
@@ -2,9 +2,69 @@ import "./App.css";
 import TaskCard from "./components/task-card/TaskCard";
 
 function App() {
+
+  // const id = "T-1";
+  // const status = "Todo";
+  // const name = "Create a Design System for Enum Workspace.";
+  // const dueDate = new Date(2023, 7 , 15);
+
+  //From the above constant declaration, we create an 
+  //array that holds objects; we do this to store 
+  //the passed parameters as dynamically as possible
+
+  const data = [
+    {
+      id: "T-1",
+      status: "Todo",
+      name: "Create a Design System for Enum Workspace.",
+      dueDate: new Date(2012, 3, 22),
+    },
+    {
+      id: "T-2",
+      status: "Todo",
+      name: "Create a Design System for Enum Workspace.",
+      dueDate: new Date(2012, 3, 22),
+    },
+    {
+      id: "T-3",
+      status: "Todo",
+      name: "Create a Design System for Enum Workspace.",
+      dueDate: new Date(2012, 3, 22),
+    },
+    {
+      id: "T-4",
+      status: "Todo",
+      name: "Create a Design System for Enum Workspace.",
+      dueDate: new Date(2012, 3, 22),
+    },
+  ]
+
   return (
     <div className="app-container">
-      <TaskCard />
+      <TaskCard 
+      id = {data[0].id}
+      status = {data[0].status}
+      name = {data[0].name}
+      dueDate = {data[0].dueDate}
+      />
+      <TaskCard 
+      id = {data[1].id}
+      status = {data[1].status}
+      name = {data[1].name}
+      dueDate = {data[1].dueDate}
+      />
+      <TaskCard 
+      id = {data[2].id}
+      status = {data[2].status}
+      name = {data[2].name}
+      dueDate = {data[2].dueDate}
+      />
+      <TaskCard 
+      id = {data[3].id}
+      status = {data[3].status}
+      name = {data[3].name}
+      dueDate = {data[3].dueDate}
+      />
     </div>
   );
 }
--- Homeworks/React/Task-Manager/src/components/task-card/TaskCard.jsx
+++ Homeworks/React/Task-Manager/src/components/task-card/TaskCard.jsx
@@ -1,25 +1,53 @@
 import "./TaskCard.css";
+import Badge from "../badge/Badge";
+import DateContainer from "../date-container/DateContainer";
 
-function TaskCard() {
+function TaskCard(props) {
+
+  //first step: 
+  //extract the elements into constants
+  // const id = "T-1";
+  // const status = "Todo";
+  // const name = "Create a Design System for Enum Workspace.";
+  // const dueDate = new Date (2023, 7 , 15);
+
+  //props => object argument with data and returns a React element
+
   return (
     <div className="card-wrapper">
       <div className="card-header">
-        <p className="task-id">T-1</p>
-        <div className="badge">
-          <p>Todo</p>
-        </div>
+        <p className="task-id"> {props.id} </p>
+        <Badge status={props.status}/>
+        {/* <div className="badge">
+          <p> {props.status} </p>
+        </div> */}
       </div>
       <div className="card-content">
-        <p>Create a Design System for Enum Workspace.</p>
+        <p> {props.name} </p>
       </div>
       <div className="card-footer">
-        <div className="due-date">
+        <DateContainer date = {props.dueDate}/>
+        {/* <div className="due-date">
           <p>Due Date</p>
-          <p>24/ 01 /2021</p>
-        </div>
+          <p> {props.dueDate.toLocaleDateString()} </p>
+        </div> */}
       </div>
     </div>
   );
 }
+
+// We can notice two components that we will manage in separate folders
+// This two components are the following:
+// <div className="badge">
+// <p> {props.status} </p>
+// </div> 
+
+// <div className="due-date">
+// <p>Due Date</p>
+// <p> {props.dueDate.toLocaleDateString()} </p>
+// </div>
+
+
+
 
 export default TaskCard;
