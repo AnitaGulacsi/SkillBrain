8dbc4568db612d57d1fb6e4272b3b082c2dcb232
Anita Gulacsi <gulacsi_anita@yahoo.com>
2023-09-19 16:20:13 +0300

    object of type state

--- Homeworks/React/Task-Manager/src/components/forms/CreateTaskForm.jsx
+++ Homeworks/React/Task-Manager/src/components/forms/CreateTaskForm.jsx
@@ -1,24 +1,141 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreateTaskForm.css";
 
 const CreateTaskForm = () => {
+    const [taskName, setTaskName] = useState("");
+    const [dueDate, setDueDate] = useState();
+    const [taskDatails, setDeatails] = useState("");
+
+    //putem folosi si un singur state:
+    const [formData, setFormDate] = useState({
+        taskName: "",
+        dueDate: "",
+        taskDetails: "",
+    });
+
+
+    // console.log(formData);
+
+    const handleNameChange = (event) => {
+       
+            // setFormDate({
+            //     ...formData, //ia tot ce e in formData si daca avem ceva nou in objectul nostru o sa fie adaugat 
+            //     taskName: event.target.value,
+            // }) 
+        
+                     
+            /* 
+                in react nu folosim in modul acesta de state-ul precedent 
+                tanand cont de natura stetului si cum el se apdateaza si cum trebuie 'sa astepte' sa se intample un process in spate 
+                nu ne putem baza facand o declaratie de genu, ca ce e aici in formData este ultimul state, mai ales la randari asa de repede
+                console.log(formData); deaoarece este pe pozitia 0 putem vedea ori de cate ori scriem ceva in form functia se apeleaza
+                si de aceea riscam sa pierdem informatii   
+            */
+                
+
+            //deaceea este un alt mod in care putem rezolva aceasta problema
+
+        setFormDate((prevState) => ({
+            ...prevState,
+            taskName: event.target.value,
+        }));
+
+        setTaskName(event.target.value);
+        console.log(event.target.value); // aflam ce valoare are 
+    } 
+
+    
+    const handleDateChange = (event) => {
+
+        setFormDate((prevState) => ({
+            ...prevState,
+            dueDate: event.target.value,
+        }));
+
+        // setDueDate(event.target.value);
+        // console.log(event.target.value); // aflam ce valoare are 
+    } 
+
+
+   
+    const handleDetailsChange = (event) => {
+
+        setFormDate((prevState) => ({
+            ...prevState,
+            taskDetails: event.target.value,
+        }));
+
+        // setDeatails(event.target.value);
+        // console.log(event.target.value); // aflam ce valoare are 
+    } 
+
+    // console.log(newTask);
+
+    //creacrea unei functii genarale 
+/*     const handleInputChange = (event) => {
+       console.log(event.target.name);
+          
+        setFormDate((prevState) => (
+            {
+                ...prevState,
+                [event.target.name]: event.target.value,
+            }
+        ));
+    }; */
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // dezactivam mini revrash-ul pe care il face form-ul default 
+        // console.log("taskName=", taskName);
+        // console.log("dueDte=", dueDate);
+        // console.log("taskDetails", taskDatails);
+        // console.log("formDate", formData);
+
+        const newTask = {
+            name: taskName,
+            dueDate: dueDate,
+            taskDetails: taskDatails,
+            status: "to do",
+        } 
+
+        console.log("newTask=", newTask);
+    };
+
 
     return (
         <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-row">
             <label className="label-md">Task Name</label>
-            <input className="input-primary" type="text" />
+            <input 
+                // value={taskName}
+                name="taskName"
+                onChange={handleNameChange} 
+                className="input-primary" 
+                type="text" 
+            />
           </div>
   
           <div className="form-row">
             <label className="label-md">Due Date</label>
-            <input className="input-primary" type="date" />
+            <input 
+                // value={douDate}
+                name="dueData"
+                onChange={handleDateChange} 
+                className="input-primary" 
+                type="date" 
+            />
           </div>
   
           <div className="form-row">
             <label className="label-md">Task Details</label>
-            <textarea className="input-primary" cols="30" rows="10"></textarea>
+            <textarea 
+                // value={taskDetails}
+                name="textDetails"
+                onChange={handleDetailsChange} 
+                className="input-primary" 
+                cols="30" 
+                rows="10">
+            </textarea>
           </div>
   
           <button className="button-primary" type="submit">
